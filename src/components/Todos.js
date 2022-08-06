ecb55bb422a7d48c736bf474c4fbca0fc4ec8264
ashsachan <ashishsachan0712@outlook.in>
2022-08-06 18:13:02 +0530

    03: Add Working with Redux.

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -21,31 +21,48 @@ const Todos = (props) => {
     setTodo(e.target.value);
   };
 
-  const add = () => {
-    if (todo === "") {
-      alert("Input is Empty");
-    } else {
-      props.addTodo({
-        id: Math.floor(Math.random() * 1000),
-        item: todo,
-        completed: false,
-      });
-      setTodo("");
-    }
-  };
-  //console.log("props from store", props);
+  // const add = () => {
+  //   if (todo === "") {
+  //     alert("Input is Empty");
+  //   } else {
+  //     props.addTodo({
+  //       id: Math.floor(Math.random() * 1000),
+  //       item: todo,
+  //       completed: false,
+  //     });
+  //     setTodo("");
+  //   }
+  // };
+
+  console.log("props from store", props);
+
   return (
     <div className="addTodos">
       <input
         type="text"
         onChange={(e) => handleChange(e)}
         className="todo-input"
-        value={todo}
       />
-      <button className="add-btn" onClick={() => add()}>
+      <button
+        className="add-btn"
+        onClick={() =>
+          props.addTodo({
+            id: Math.floor(Math.random() * 1000),
+            item: todo,
+            completed: false,
+          })
+        }
+      >
         Add
       </button>
+
       <br />
+
+      <ui>
+        {props.todos.map((item) => {
+          return <li key={item.id}> {item.item}</li>;
+        })}
+      </ui>
     </div>
   );
 };
